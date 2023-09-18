53dd0d559a98b614d736534f89120b79470e7174
vinayytyagi <113412199+vinayytyagi@users.noreply.github.com>
2023-09-18 19:06:32 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
